4e10f5baedb76f4d1583037dc2070996ad3a080d
Олександр Радчук <73553177+brolo1313@users.noreply.github.com>
2022-01-11 22:19:41 +0200

    remove & update product dashboard logic

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ProductService } from 'src/app/shared/product.service';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardPageComponent implements OnInit {
 
-  constructor() { }
+  products:any=[]
+  pSub:Subscription | undefined
+  rSub:Subscription | undefined
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.pSub = this.productService.getAll().subscribe( products => {
+      this.products = products;
+    })
+  }
+
+  ngOnDestroy(){
+    if(this.pSub){
+      this.pSub.unsubscribe();
+    }
+
+    if(this.rSub){
+      this.rSub.unsubscribe();
+    }
+  }
+
+  remove(id:any){
+    this.rSub = this.productService.remove(id).subscribe(() => {
+      this.products = this.products.filter((product:any) => {
+        product.id !== id
+        console.log('id елемента которий удаляем:' + id);
+        console.log('сравниваем id которий в базе:' + product);
+      })
+    })
   }
 
 }
