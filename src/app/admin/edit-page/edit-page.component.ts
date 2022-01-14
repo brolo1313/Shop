120bcc1b4f030bf5aa0f1a485dc6f46312804713
Олександр Радчук <73553177+brolo1313@users.noreply.github.com>
2022-01-14 17:34:10 +0200

    added logic for edit product

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,15 +1,65 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { Product } from 'src/app/shared/interfaces';
+import { ProductService } from 'src/app/shared/product.service';
 
 @Component({
   selector: 'app-edit-page',
   templateUrl: './edit-page.component.html',
-  styleUrls: ['./edit-page.component.scss']
+  styleUrls: ['./edit-page.component.scss'],
 })
 export class EditPageComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
+  product!: Product ;
 
-  ngOnInit(): void {
+  isSubmitted = false;
+
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService,
+    private router: Router
+  ) {}
+
+  ngOnInit(){
+    this.route.params.pipe(
+      switchMap(params => {
+        return this.productService.getById(params['id'])
+      })
+    ).subscribe(product => {
+      this.product = product;
+      this.form = new FormGroup({
+        type:new FormControl(this.product.type, [Validators.required]),
+        title:new FormControl(this.product.title, [Validators.required]),
+        photo:new FormControl(this.product.photo, [Validators.required]),
+        info:new FormControl(this.product.info, [Validators.required]),
+        price:new FormControl(this.product.price, [Validators.required])
+      })
+    })
   }
 
+
+  submit(){
+    if(this.form.invalid){
+      return;
+    }
+    
+    this.isSubmitted = true;
+
+  
+    this.productService.update({
+      ...this.product,
+      type:this.form.value.type,
+      title:this.form.value.title,
+      photo:this.form.value.photo,
+      info:this.form.value.info,
+      price:this.form.value.price,
+      date:new Date()
+    }).subscribe(res => {
+     this.isSubmitted = false;
+     this.router.navigate(['/admin/dashboard'])
+    })
+  }
 }
